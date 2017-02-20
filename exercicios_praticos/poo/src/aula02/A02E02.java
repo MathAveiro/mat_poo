ce6e8e74ddd9b1a2842dce66ef2cda252d4c3153
fabioHfab <fabiomchenriques@ua.pt>
2017-02-20 17:36:41 +0000

    Exercícios resolvidos aula02

--- exercicios_praticos/poo/src/aula02/A02E02.java
+++ exercicios_praticos/poo/src/aula02/A02E02.java
@@ -6,10 +6,10 @@ public class A02E02 {
 	static Scanner sc =  new Scanner(System.in);
 	public static void main(String[] args) {
 		int n=1;
-		while(n<=0) {
+		 do{
 			out.print("Insira um número positivo inteiro: ");
 			n = sc.nextInt();
-		}
+		}while(n<=0);
 		for(int i = n; i >= 0; i--) {
 			out.println(i);
 		}
